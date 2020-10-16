2993f5ed5bf008885a3fe4bf4600dbf4a40e7391
Michael Richardson <mirichardson@paloaltonetworks.com>
2020-10-16 11:26:57 -0400

    feat: Add bootstrap module

---
+++ modules/bootstrap/main.tf
@@ -0,0 +1,106 @@
+############################################################################################
+# Copyright 2020 Palo Alto Networks.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+############################################################################################
+
+
+resource "random_id" "bucket_id" {
+  byte_length = 8
+}
+resource "aws_s3_bucket" "bucket" {
+  bucket = "bootstrap-${random_id.bucket_id.hex}"
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_object" "bootstrap_dirs" {
+  for_each = toset(var.bootstrap_directories)
+
+  bucket  = aws_s3_bucket.bucket.id
+  key     = each.value
+  content = "/dev/null"
+}
+
+resource "aws_s3_bucket_object" "init_cfg" {
+  bucket = aws_s3_bucket.bucket.id
+  key    = "config/init-cfg.txt"
+  content = templatefile("${path.module}/init-cfg.txt.tmpl",
+    {
+      "hostname"         = var.hostname,
+      "panorama-server"  = var.panorama-server,
+      "panorama-server2" = var.panorama-server2,
+      "tplname"          = var.tplname,
+      "dgname"           = var.dgname,
+      "dns-primary"      = var.dns-primary,
+      "dns-secondary"    = var.dns-secondary,
+      "vm-auth-key"      = var.vm-auth-key,
+      "op-command-modes" = var.op-command-modes
+    }
+  )
+}
+
+resource "aws_s3_bucket_object" "bootstrap_files" {
+  for_each = fileset("${path.root}/files", "**")
+
+  bucket = aws_s3_bucket.bucket.id
+  key    = each.value
+  source = "${path.root}/files/${each.value}"
+}
+
+resource "aws_iam_role" "bootstrap" {
+  name = "BootstrapRole-${random_id.bucket_id.hex}"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+      "Service": "ec2.amazonaws.com"
+    },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "bootstrap" {
+  name = "BootstrapRolePolicy-${random_id.bucket_id.hex}"
+  role = aws_iam_role.bootstrap.id
+
+  policy = <<EOF
+{
+  "Version" : "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "s3:ListBucket",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.bucket.bucket}"
+    },
+    {
+    "Effect": "Allow",
+    "Action": "s3:GetObject",
+    "Resource": "arn:aws:s3:::${aws_s3_bucket.bucket.bucket}/*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_instance_profile" "bootstrap" {
+  name = "BootstrapInstanceProfile-${random_id.bucket_id.hex}"
+  role = aws_iam_role.bootstrap.name
+  path = "/"
+}
---
+++ modules/bootstrap/variables.tf
@@ -0,0 +1,81 @@
+############################################################################################
+# Copyright 2020 Palo Alto Networks.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+############################################################################################
+
+
+variable "bootstrap_directories" {
+  description = "The directories comprising the bootstrap package"
+  default = [
+    "config/",
+    "content/",
+    "software/",
+    "license/",
+    "plugins/"
+  ]
+}
+
+variable "hostname" {
+  default     = ""
+  description = "The hostname of the VM-series instance"
+  type        = string
+}
+
+variable "panorama-server" {
+  default     = ""
+  description = "The FQDN or IP address of the primary Panorama server"
+  type        = string
+}
+
+variable "panorama-server2" {
+  default     = ""
+  description = "The FQDN or IP address of the secondary Panorama server"
+  type        = string
+}
+
+variable "tplname" {
+  default     = ""
+  description = "The Panorama template stack name"
+  type        = string
+}
+
+variable "dgname" {
+  default     = ""
+  description = "The Panorama device group name"
+  type        = string
+}
+
+variable "dns-primary" {
+  default     = ""
+  description = "The IP address of the primary DNS server"
+  type        = string
+}
+
+variable "dns-secondary" {
+  default     = ""
+  description = "The IP address of the secondary DNS server"
+  type        = string
+}
+
+variable "vm-auth-key" {
+  default     = ""
+  description = "Virtual machine authentication key"
+  type        = string
+}
+
+variable "op-command-modes" {
+  default     = ""
+  description = "Set jumbo-frame and/or mgmt-interface-swap"
+  type        = string
+}
