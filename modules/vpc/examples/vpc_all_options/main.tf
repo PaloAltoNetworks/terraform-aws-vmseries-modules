2d02692ba065afb6dd086b83060517e04242a4bf
Sean Youngberg <syoungberg@paloaltonetworks.com>
2020-10-19 10:10:51 -0400

    separate provider requirements to versions.tf

--- modules/vpc/examples/vpc_all_options/main.tf
+++ modules/vpc/examples/vpc_all_options/main.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_version = "~> 0.13"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3"
-    }
-  }
-}
-
-provider "aws" {
-  region  = var.region
-  version = "3.8.0"
-  profile = "default"
-}
-
 module "vpc_all_options" {
   source           = "../../"
   global_tags      = var.global_tags
