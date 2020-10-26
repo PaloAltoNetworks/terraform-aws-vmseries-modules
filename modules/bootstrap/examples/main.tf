28aff52901349dbdeb103cdf0f9b55f1d08011f8
Konrad Bechler <konrad@konsol.eu>
2020-10-26 11:10:19 +0100

    new versions.tf file

--- modules/bootstrap/examples/main.tf
+++ modules/bootstrap/examples/main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_version = "~>0.13, <0.14"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3"
-    }
-  }
-}
-
 provider "aws" {
   region  = var.region
   version = "3.8.0"
