b924f5c71322476a38c805ebf96c3f2caa6ca5d8
Sean Youngberg <syoungberg@paloaltonetworks.com>
2020-11-06 13:50:05 -0500

    fix required_providers instead of provider block in modules. compatible with 0.12.26+

--- examples/single_vpc/versions.tf
+++ examples/single_vpc/versions.tf
@@ -1,8 +1,13 @@
 terraform {
   required_version = ">=0.12.29, <0.14"
+  required_providers {
+    aws = {
+      version = "~> 3.10"
+    }
+  }
 }
 
 provider "aws" {
-  version = "~> 3"
+  version = "~> 3.10"
   region  = var.region
 }
--- modules/vpc_routes/versions.tf
+++ modules/vpc_routes/versions.tf
@@ -1,8 +1,8 @@
 terraform {
   required_version = ">=0.12.29, <0.14"
-}
-
-provider "aws" {
-  version = "~> 3"
-  region  = var.region
+  required_providers {
+    aws = {
+      version = "~> 3.10"
+    }
+  }
 }
