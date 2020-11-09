842758a542c0e0eb51543279c4e411a1c8d9500b
Sean Youngberg <syoungberg@paloaltonetworks.com>
2020-11-09 01:04:03 -0500

    fixes for other module examples versions.tf

--- modules/vpc/examples/vpc_all_options/versions.tf
+++ modules/vpc/examples/vpc_all_options/versions.tf
@@ -2,14 +2,11 @@ terraform {
   required_version = ">=0.12.29, <0.14"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3"
+      version = "~> 3.10"
     }
   }
 }
 
 provider "aws" {
   region  = var.region
-  version = "3.8.0"
-  profile = "default"
 }
