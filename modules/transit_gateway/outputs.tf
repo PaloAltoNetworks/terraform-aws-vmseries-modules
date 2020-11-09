ddab661918871b8fd21277ddfead990888a6356b
Sean Youngberg <syoungberg@paloaltonetworks.com>
2020-11-09 01:53:43 -0500

    fix fmt

--- modules/transit_gateway/outputs.tf
+++ modules/transit_gateway/outputs.tf
@@ -5,5 +5,5 @@ output transit_gateway_ids {
 
 output "transit_gateway_route_table_ids" {
   description = "TGW Route Table Name -> ID Map (New AND Existing)"
-  value = local.combined_transit_gateway_route_tables
+  value       = local.combined_transit_gateway_route_tables
 }
--- modules/vpc_routes/examples/all_options/versions.tf
+++ modules/vpc_routes/examples/all_options/versions.tf
@@ -8,5 +8,5 @@ terraform {
 }
 
 provider "aws" {
-  region  = var.region
+  region = var.region
 }
