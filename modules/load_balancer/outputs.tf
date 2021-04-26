83e44b49c769c276523a470df9d4e87126cfbd69
Konrad Bechler <kbechler@paloaltonetworks.com>
2021-04-26 13:15:18 +0200

    output and versioning

--- modules/load_balancer/outputs.tf
+++ modules/load_balancer/outputs.tf
@@ -1,17 +1,13 @@
-#output "nlb_eips" {
-#  value = {
-#    for eip in aws_eip.nlb:
-#    eip.tags.Name => eip.public_ip
-#  }
-#}
+output "nlb_eips" {
+ value = {
+    for k, v in aws_eip.nlb:
+    k => v.public_ip
+  }
+}
 
 # output "alb_dns" {
 #   value = {
 #     for alb in aws_lb.alb :
 #     alb.name => alb.dns_name
 #   }
-# }
-
-# output "panos_warning" {
-#   value = "Changes to Panorama have not been committed. Please review and commit and push changes."
 # }
