e67a2580ee999002fb19899a521495b3f0680edc
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2021-05-14 15:21:28 +0200

    refactor: Check required_providers with tflint
    Always require `required_providers` and `required_versions` with tflint
    rules. Add these where they were missing.
    
    Move `provider {}` into versions.tf for coherence with the rest of the
    examples.

--- examples/single_vpc/versions.tf
+++ examples/single_vpc/versions.tf
@@ -2,12 +2,12 @@ terraform {
   required_version = ">=0.12.29, <0.14"
   required_providers {
     aws = {
+      source  = "hashicorp/aws"
       version = "~> 3.10"
     }
   }
 }
 
 provider "aws" {
-  version = "~> 3.10"
-  region  = var.region
+  region = var.region
 }
--- modules/vmseries/examples/vmseries_with_bootstrap_userdata/providers.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-  region  = var.region
-  version = "3.20.0"
-  profile = "default"
-}
