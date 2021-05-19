5ceec23d2e132d6141b4a0f236f8c00be9ea3305
Sean Youngberg <syoungberg@paloaltonetworks.com>
2021-05-19 03:24:47 -0400

    Add descriptions and syntax standardization for outputs. Add full outputs for nlb / alb

--- modules/load_balancer/outputs.tf
+++ modules/load_balancer/outputs.tf
@@ -1,13 +1,33 @@
-output "nlb_eips" {
+output nlb_eips {
+  description = "Map of EIPs created for NLBs"
   value = {
     for k, v in aws_eip.nlb :
     k => v.public_ip
   }
 }
 
-# output "alb_dns" {
-#   value = {
-#     for alb in aws_lb.alb :
-#     alb.name => alb.dns_name
-#   }
-# }
+output alb_dns {
+  description = "Map of DNS Names for each ALB"
+  value = {
+    for k, v in aws_lb.alb :
+    k => v.dns_name
+  }
+}
+
+output nlb_dns {
+  description = "Map of DNS Names for each NLB"
+  value = {
+    for k, v in aws_lb.nlb :
+    k => v.dns_name
+  }
+}
+
+output nlbs {
+  description = "Full output of all NLBs"
+  value = aws_lb.nlb
+}
+
+output albs {
+  description = "Full output of all ALBs"
+  value = aws_lb.alb
+}
