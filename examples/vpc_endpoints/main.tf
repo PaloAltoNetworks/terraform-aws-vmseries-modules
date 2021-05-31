5bc8585bfe85e2896277595759f9b13b10612261
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2021-05-31 16:18:33 +0200

    add subnet_set

--- examples/vpc_endpoints/main.tf
+++ examples/vpc_endpoints/main.tf
@@ -3,31 +3,42 @@ module "vpc" {
 
   create_vpc              = true
   name                    = "example2-vpc"
+  availability_zones      = ["us-east-1a", "us-east-1b"]
   cidr_block              = "10.100.0.0/16"
   secondary_cidr_blocks   = ["10.200.0.0/16", "10.201.0.0/16"]
   create_internet_gateway = true
   create_vpn_gateway      = true
   global_tags             = var.global_tags
+  security_groups         = var.security_groups # Needed only for apigw-endpoint.
 }
 
-module "subnet" {
-  source = "../../modules/subnet"
+module "subnet_public" {
+  source = "../../modules/subnet_set"
 
-  name                  = "mgmt-1a"
+  name                  = "public-1"
   vpc                   = module.vpc
-  cidr_block            = "10.100.0.0/25"
-  availability_zone     = "us-east-1a"
+  cidr_blocks           = ["10.100.0.0/25", "10.100.64.0/25"]
   create_route_table    = true
-  propagate_routes_from = { myvpn = module.vpc.vpn_gateway.id /* Optionally also a Detached VGW (that is, not parented by a VPC). */ }
+  create_nat_gateway    = true
+  propagate_routes_from = { myvpn = module.vpc.vpn_gateway.id /* Optionally also a Detached VGW (that is, one not parented by a VPC). */ }
 
   routes = {
     mgmt-igw = {
       prefix        = "0.0.0.0/0"
       next_hop_type = "internet_gateway"
     }
+    # FIXME nat_gateway
   }
   vpc_endpoints = {
+    apigw-endpoint = {
+      create             = true
+      name               = "apigw-endpoint"
+      service_name       = "com.amazonaws.us-east-1.execute-api"
+      vpc_endpoint_type  = "Interface"
+      security_group_ids = [module.vpc.security_group_ids["vpc-endpoint"]]
+    }
     s3-endpoint = {
+      create            = true
       name              = "s3-endpoint"
       service_name      = "com.amazonaws.us-east-1.s3"
       vpc_endpoint_type = "Gateway"
--- tests/vpc_existing/main.tf
+++ tests/vpc_existing/main.tf
@@ -1,24 +1,36 @@
 variable switch {}
 
+data "aws_availability_zones" "this" {
+  state = "available"
+}
+
 module "vpc" {
   source = "../../modules/vpc"
 
   create_vpc              = true
   name                    = "test3-vpc3" # TODO use random_id for a minor test: non-static name
   cidr_block              = "10.105.0.0/16"
+  availability_zones      = [data.aws_availability_zones.this.names[2], data.aws_availability_zones.this.names[3]] # ["us-east-1c", "us-east-1d"] # FIXME 0.12 Invalid for_each: [data.aws_availability_zones.this.names[2], data.aws_availability_zones.this.names[3]]
   create_internet_gateway = false
   enable_dns_hostnames    = var.switch
   global_tags             = { "Is DNS Enabled" = var.switch }
 }
 
-module "subnet" {
-  source = "../../modules/subnet"
+# FIXME 0.12 cannot destroy it:
+# Error: Cycle: module.added_subnet_set.aws_subnet.this["d"] (destroy)
+# module.existing_vpc.aws_internet_gateway.this[0] (destroy)
+# module.added_subnet_set.aws_subnet.this["c"] (destroy)
+# module.existing_vpc.data.aws_vpc.this[0] (destroy)
+# module.added_subnet_set.var.vpc
+# module.added_subnet_set.local.subnets
+
+module "subnet_set" {
+  source = "../../modules/subnet_set"
 
   create_subnet           = true
-  name                    = "mgmt-1a"
+  name                    = "mgmt-1"
   vpc                     = module.vpc
-  cidr_block              = "10.105.0.0/25"
-  availability_zone       = "us-east-1a"
+  cidr_blocks             = ["10.105.0.0/25", "10.105.1.0/25"]
   map_public_ip_on_launch = var.switch
   global_tags             = { "Is Public Mapping Enabled" = var.switch }
 }
@@ -30,31 +42,33 @@ module "existing_vpc" {
 
   create_vpc              = false
   name                    = module.vpc.name
+  availability_zones      = [data.aws_availability_zones.this.names[2], data.aws_availability_zones.this.names[3]]
   create_internet_gateway = true
 }
 
-module "existing_subnet" {
-  source = "../../modules/subnet"
+module "existing_subnet_set" {
+  source = "../../modules/subnet_set"
 
-  create_subnet = false
-  vpc           = module.existing_vpc # core test: can existing_vpc module report a correct vpc.id
-  name          = module.subnet.name
+  create_subnet         = false
+  existing_subnet_names = module.subnet_set.names
+  vpc                   = module.existing_vpc # core test: can existing_vpc module report a correct vpc.id
 }
 
-module "added_subnet" {
-  source = "../../modules/subnet"
+module "added_subnet_set" {
+  source = "../../modules/subnet_set"
 
   create_subnet = true
+  name          = "added-"
   vpc           = module.existing_vpc
-  cidr_block    = "10.105.5.0/25"
+  cidr_blocks   = ["10.105.5.0/25", "10.105.6.0/25"]
 }
 
 ### Test Results ###
 
 output "is_subnet_cidr_correct" {
-  value = (try(module.existing_subnet.subnet.cidr_block, null) == "10.105.0.0/25")
+  value = (try(module.existing_subnet_set.subnets["c"].cidr_block, null) == "10.105.0.0/25")
 }
 
 output "is_subnet_name_correct" {
-  value = (module.added_subnet.name == null)
+  value = (try(module.added_subnet_set.names[1], null) == "added-d")
 }
