ae525f6f07480acb2c1ae9a5fa32884da0ec2095
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2021-06-29 11:11:18 +0200

    subnet_set remove input prefix_name_tag

--- modules/subnet_set/variables.tf
+++ modules/subnet_set/variables.tf
@@ -1,13 +1,13 @@
 variable name { default = null }
 variable cidrs {}
-variable prefix_name_tag { default = "prefix-" }
-variable create_shared_route_table { default = false }
-variable global_tags { default = {} }
-variable map_public_ip_on_launch { default = null }
 variable vpc_id {}
+variable create_shared_route_table { default = false }
+variable map_public_ip_on_launch { default = null }
 
 variable propagating_vgws {
   description = "See the [provider's documentation](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table)."
   default     = []
   type        = list(string)
 }
+
+variable global_tags { default = {} }
