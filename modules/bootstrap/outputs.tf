9411d7c9ae804a18006b5ff39c787908c199acc8
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2021-07-14 13:29:46 +0200

    add bootstrap output bucket_domain_name

--- modules/bootstrap/outputs.tf
+++ modules/bootstrap/outputs.tf
@@ -1,11 +1,21 @@
 output "bucket_id" {
   value       = aws_s3_bucket.this.id
-  description = "ID of created bucket."
+  description = "AWS identifier of the created bucket."
 }
 
 output "bucket_name" {
   value       = aws_s3_bucket.this.bucket
-  description = "Name of created bucket."
+  description = "Name of the created bucket."
+}
+
+output "bucket_domain_name" {
+  value       = aws_s3_bucket.this.bucket_domain_name
+  description = "Global domain name of the created bucket."
+}
+
+output "bucket_regional_domain_name" {
+  value       = aws_s3_bucket.this.bucket_regional_domain_name
+  description = "Regional domain name of the created bucket."
 }
 
 output "instance_profile_name" {
