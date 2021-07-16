2d4c73cea5e8fd47a434bed24102b3862de1edc2
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2021-07-16 16:14:10 +0200

    add initial terratest for bootstrap module (#52)
    * test(bootstrap): new terratest-based code
    
    * add bootstrap output bucket_domain_name
    
    * add tests/bootstrap http.Get of the S3 bucket
    
    * formatting

---
+++ tests/bootstrap/main.tf
@@ -0,0 +1,28 @@
+variable region {
+  description = "AWS region to use for the created resources."
+  default     = null
+  type        = string
+}
+
+variable switchme {
+  description = "The true/false switch for testing the modifiability. Initial runs should use `true`, then at some point one or more consecutive runs should use `false` instead."
+  type        = bool
+}
+
+module "bootstrap" {
+  source      = "../../modules/bootstrap"
+  prefix      = "a"
+  global_tags = var.switchme ? {} : { switchme = var.switchme }
+}
+
+output bucket_name_correct {
+  value = (substr(module.bootstrap.bucket_name, 0, 2) == "a-")
+}
+
+output instance_profile_name_correct {
+  value = (substr(module.bootstrap.instance_profile_name, 0, 2) == "a-")
+}
+
+output bucket_domain_name {
+  value = module.bootstrap.bucket_domain_name
+}
