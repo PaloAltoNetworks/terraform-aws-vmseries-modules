5cd7d8d16bc749145a279e40d71d21296780eac4
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2021-07-23 17:43:43 +0200

    fix bootstrap: Does not support GWLB settings, too rigid.

--- modules/bootstrap/main.tf
+++ modules/bootstrap/main.tf
@@ -3,7 +3,7 @@ resource "random_id" "bucket_id" {
 }
 
 resource "aws_s3_bucket" "this" {
-  bucket = "${var.prefix}-${random_id.bucket_id.hex}"
+  bucket = "${var.prefix}${random_id.bucket_id.hex}"
   acl    = "private"
   tags   = var.global_tags
 }
@@ -16,24 +16,6 @@ resource "aws_s3_bucket_object" "bootstrap_dirs" {
   content = "/dev/null"
 }
 
-resource "aws_s3_bucket_object" "init_cfg" {
-  bucket = aws_s3_bucket.this.id
-  key    = "config/init-cfg.txt"
-  content = templatefile("${path.module}/init-cfg.txt.tmpl",
-    {
-      "hostname"         = var.hostname,
-      "panorama-server"  = var.panorama-server,
-      "panorama-server2" = var.panorama-server2,
-      "tplname"          = var.tplname,
-      "dgname"           = var.dgname,
-      "dns-primary"      = var.dns-primary,
-      "dns-secondary"    = var.dns-secondary,
-      "vm-auth-key"      = var.vm-auth-key,
-      "op-command-modes" = var.op-command-modes
-    }
-  )
-}
-
 resource "aws_s3_bucket_object" "bootstrap_files" {
   for_each = fileset("${path.root}/files", "**")
 
@@ -43,7 +25,7 @@ resource "aws_s3_bucket_object" "bootstrap_files" {
 }
 
 resource "aws_iam_role" "this" {
-  name = "${var.prefix}-${random_id.bucket_id.hex}"
+  name = "${var.prefix}${random_id.bucket_id.hex}"
 
   tags               = var.global_tags
   assume_role_policy = <<EOF
@@ -63,7 +45,7 @@ EOF
 }
 
 resource "aws_iam_role_policy" "bootstrap" {
-  name   = "${var.prefix}-${random_id.bucket_id.hex}"
+  name   = "${var.prefix}${random_id.bucket_id.hex}"
   role   = aws_iam_role.this.id
   policy = <<EOF
 {
@@ -85,7 +67,7 @@ EOF
 }
 
 resource "aws_iam_instance_profile" "this" {
-  name = coalesce(var.iam_instance_profile_name, "${var.prefix}-${random_id.bucket_id.hex}")
+  name = coalesce(var.iam_instance_profile_name, "${var.prefix}${random_id.bucket_id.hex}")
   role = aws_iam_role.this.name
   path = "/"
 }
