4435dc83f5b7080fe2e31702242af5553928b1f0
Damian Fedeczko <dfedeczko@paloaltonetworks.com>
2021-08-03 11:38:22 +0200

    Fix Panorama

--- modules/panorama/examples/all_options/variables.tf
+++ modules/panorama/examples/all_options/variables.tf
@@ -1,18 +1,12 @@
 # Check module for variable definitions and documentation
 
-variable region {
-  default = ""
-}
+variable "region" {}
 
-variable global_tags {
-  type        = map(any)
-  description = "A map of tags to add to all resources"
-  default     = {}
-}
+variable "global_tags" {}
 
-variable panorama_version {
+variable "panorama_version" {
   type    = string
   default = "10.0.2"
 }
 
-variable panoramas {}
+variable "panoramas" {}
