663c0486e43b3b1c9989931beeb569cb252e6c28
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2021-09-29 14:39:22 +0200

    fix comment

--- tests/vpc_plan/main_test.go
+++ tests/vpc_plan/main_test.go
@@ -17,6 +17,7 @@ func TestMain(t *testing.T) {
 	// Schedule `terraform destroy` at the end of the test, to clean up the created resources.
 	defer terraform.Destroy(t, terraformOptions)
 
-	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors.
+	// This will run `terraform init` and `terraform plan` and fail the test if there are any errors.
+	// This specific test is not intended to execute `terraform apply` at all.
 	terraform.InitAndPlan(t, terraformOptions)
 }
