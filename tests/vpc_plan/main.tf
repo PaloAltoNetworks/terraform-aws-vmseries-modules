58b7bde0f7cfe3f9fd1a2949b8061158e9461a38
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2021-10-22 09:43:11 +0200

    tests/vpc_plan simplify
    Do not repeat those `security_groups`.
    
    Less test cases. Select what is likely to cause trouble instead of going
    through all 2^4 combinations (discourage such exponential code growth in
    future).

--- tests/vpc_plan/main.tf
+++ tests/vpc_plan/main.tf
@@ -17,18 +17,8 @@ locals {
     # It is unknown whether the merge is a single-element map or a two-element map.
   )
   u_sg_rule_type = local.u_bool ? "egress" : "ingress"
-}
 
-### The code under test. ###
-
-module "vpc" {
-  source = "../../modules/vpc"
-
-  # Inputs that cannot handle unknown values.
-  create_vpc              = true
-  cidr_block              = "10.0.0.0/24"
-  secondary_cidr_blocks   = ["10.0.1.0/24", "10.0.2.0/24"]
-  create_internet_gateway = true
+  # Just for brevity:
   security_groups = {
     sg = {
       rules = {
@@ -41,6 +31,20 @@ module "vpc" {
       name = local.u_string
     }
   }
+}
+
+### The code under test. ###
+
+module "vpc" {
+  source = "../../modules/vpc"
+
+  # Inputs that cannot handle unknown values.
+  create_vpc              = true
+  cidr_block              = "10.0.0.0/24"
+  secondary_cidr_blocks   = ["10.0.1.0/24", "10.0.2.0/24"]
+  create_internet_gateway = true
+  create_vpn_gateway      = true
+  security_groups         = local.security_groups
 
   # Inputs that can handle unknown values.
   name                             = local.u_string
@@ -53,6 +57,100 @@ module "vpc" {
   vpc_tags                         = local.u_map
 }
 
+# More more variants of the same call.
+# Above we had all the possible "create" flags to true, for the most complex case.
+# Below replace each "true" with "false", one by one. It should detect unwanted dependencies.
+
+module "vpc_novgw" {
+  source = "../../modules/vpc"
+
+  # Inputs that cannot handle unknown values.
+  create_vpc              = true
+  cidr_block              = "10.0.0.0/24"
+  secondary_cidr_blocks   = ["10.0.1.0/24", "10.0.2.0/24"]
+  create_internet_gateway = true
+  create_vpn_gateway      = false # the change
+  security_groups         = local.security_groups
+
+  # Inputs that can handle unknown values.
+  name                             = local.u_string
+  enable_dns_hostnames             = local.u_bool
+  enable_dns_support               = local.u_bool
+  assign_generated_ipv6_cidr_block = local.u_bool
+  instance_tenancy                 = local.u_string
+  vpn_gateway_amazon_side_asn      = local.u_number
+  global_tags                      = local.u_map
+  vpc_tags                         = local.u_map
+}
+
+module "vpc_noigw" {
+  source = "../../modules/vpc"
+
+  # Inputs that cannot handle unknown values.
+  create_vpc              = true
+  cidr_block              = "10.0.0.0/24"
+  secondary_cidr_blocks   = ["10.0.1.0/24", "10.0.2.0/24"]
+  create_internet_gateway = false # the change
+  use_internet_gateway    = false # the change
+  create_vpn_gateway      = true
+  security_groups         = local.security_groups
+
+  # Inputs that can handle unknown values.
+  name                             = local.u_string
+  enable_dns_hostnames             = local.u_bool
+  enable_dns_support               = local.u_bool
+  assign_generated_ipv6_cidr_block = local.u_bool
+  instance_tenancy                 = local.u_string
+  vpn_gateway_amazon_side_asn      = local.u_number
+  global_tags                      = local.u_map
+  vpc_tags                         = local.u_map
+}
+
+module "vpc_useigw" {
+  source = "../../modules/vpc"
+
+  # Inputs that cannot handle unknown values.
+  create_vpc              = true
+  cidr_block              = "10.0.0.0/24"
+  secondary_cidr_blocks   = ["10.0.1.0/24", "10.0.2.0/24"]
+  create_internet_gateway = false
+  use_internet_gateway    = true # the change
+  create_vpn_gateway      = true
+  security_groups         = local.security_groups
+
+  # Inputs that can handle unknown values.
+  name                             = local.u_string
+  enable_dns_hostnames             = local.u_bool
+  enable_dns_support               = local.u_bool
+  assign_generated_ipv6_cidr_block = local.u_bool
+  instance_tenancy                 = local.u_string
+  vpn_gateway_amazon_side_asn      = local.u_number
+  global_tags                      = local.u_map
+  vpc_tags                         = local.u_map
+}
+
+module "novpc" {
+  source = "../../modules/vpc"
+
+  # Inputs that cannot handle unknown values.
+  create_vpc              = false # the change
+  create_internet_gateway = true
+  create_vpn_gateway      = true
+  security_groups         = local.security_groups
+
+  # Inputs that can handle unknown values.
+  name                             = local.u_string
+  enable_dns_hostnames             = local.u_bool
+  enable_dns_support               = local.u_bool
+  assign_generated_ipv6_cidr_block = local.u_bool
+  instance_tenancy                 = local.u_string
+  vpn_gateway_amazon_side_asn      = local.u_number
+  global_tags                      = local.u_map
+  vpc_tags                         = local.u_map
+}
+
+# For the known values that are lists or maps, check if they can be empty, one by one.
+
 module "vpc_nosg" {
   source = "../../modules/vpc"
 
@@ -61,6 +159,7 @@ module "vpc_nosg" {
   cidr_block              = "10.0.0.0/24"
   secondary_cidr_blocks   = ["10.0.1.0/24", "10.0.2.0/24"]
   create_internet_gateway = true
+  create_vpn_gateway      = true
   security_groups         = {} # the change
 
   # Inputs that can handle unknown values.
@@ -82,195 +181,6 @@ module "vpc_nosecond" {
   cidr_block              = "10.0.0.0/24"
   secondary_cidr_blocks   = [] # the change
   create_internet_gateway = true
-  security_groups = {
-    sg = {
-      rules = {
-        r = {
-          # Nested attributes that can handle unknown values.
-          type = local.u_sg_rule_type
-        }
-      }
-      # Nested attributes that can handle unknown values.
-      name = local.u_string
-    }
-  }
-
-  # Inputs that can handle unknown values.
-  name                             = local.u_string
-  enable_dns_hostnames             = local.u_bool
-  enable_dns_support               = local.u_bool
-  assign_generated_ipv6_cidr_block = local.u_bool
-  instance_tenancy                 = local.u_string
-  vpn_gateway_amazon_side_asn      = local.u_number
-  global_tags                      = local.u_map
-  vpc_tags                         = local.u_map
-}
-
-module "vpc_noigw" {
-  source = "../../modules/vpc"
-
-  # Inputs that cannot handle unknown values.
-  create_vpc              = true
-  cidr_block              = "10.0.0.0/24"
-  secondary_cidr_blocks   = ["10.0.1.0/24", "10.0.2.0/24"]
-  create_internet_gateway = false # the change
-  security_groups = {
-    sg = {
-      rules = {
-        r = {
-          # Nested attributes that can handle unknown values.
-          type = local.u_sg_rule_type
-        }
-      }
-      # Nested attributes that can handle unknown values.
-      name = local.u_string
-    }
-  }
-
-  # Inputs that can handle unknown values.
-  name                             = local.u_string
-  enable_dns_hostnames             = local.u_bool
-  enable_dns_support               = local.u_bool
-  assign_generated_ipv6_cidr_block = local.u_bool
-  instance_tenancy                 = local.u_string
-  vpn_gateway_amazon_side_asn      = local.u_number
-  global_tags                      = local.u_map
-  vpc_tags                         = local.u_map
-}
-
-module "novpc_igw" {
-  source = "../../modules/vpc"
-
-  # Inputs that cannot handle unknown values.
-  create_vpc              = false # the change
-  create_internet_gateway = true
-  security_groups = {
-    sg = {
-      rules = {
-        r = {
-          # Nested attributes that can handle unknown values.
-          type = local.u_sg_rule_type
-        }
-      }
-      # Nested attributes that can handle unknown values.
-      name = local.u_string
-    }
-  }
-
-  # Inputs that can handle unknown values.
-  name                             = module.vpc.name
-  enable_dns_hostnames             = local.u_bool
-  enable_dns_support               = local.u_bool
-  assign_generated_ipv6_cidr_block = local.u_bool
-  instance_tenancy                 = local.u_string
-  vpn_gateway_amazon_side_asn      = local.u_number
-  global_tags                      = local.u_map
-  vpc_tags                         = local.u_map
-}
-
-module "novpc_noigw" {
-  source = "../../modules/vpc"
-
-  # Inputs that cannot handle unknown values.
-  create_vpc              = false
-  create_internet_gateway = false # the change
-  use_internet_gateway    = true
-  security_groups = {
-    sg = {
-      rules = {
-        r = {
-          # Nested attributes that can handle unknown values.
-          type = local.u_sg_rule_type
-        }
-      }
-      # Nested attributes that can handle unknown values.
-      name = local.u_string
-    }
-  }
-
-  # Inputs that can handle unknown values.
-  name                             = module.vpc.name
-  enable_dns_hostnames             = local.u_bool
-  enable_dns_support               = local.u_bool
-  assign_generated_ipv6_cidr_block = local.u_bool
-  instance_tenancy                 = local.u_string
-  vpn_gateway_amazon_side_asn      = local.u_number
-  global_tags                      = local.u_map
-  vpc_tags                         = local.u_map
-}
-
-module "novpc_noigw_nouse" {
-  source = "../../modules/vpc"
-
-  # Inputs that cannot handle unknown values.
-  create_vpc              = false
-  create_internet_gateway = false
-  use_internet_gateway    = false # the change
-  security_groups = {
-    sg = {
-      rules = {
-        r = {
-          # Nested attributes that can handle unknown values.
-          type = local.u_sg_rule_type
-        }
-      }
-      # Nested attributes that can handle unknown values.
-      name = local.u_string
-    }
-  }
-
-  # Inputs that can handle unknown values.
-  name                             = module.vpc.name
-  enable_dns_hostnames             = local.u_bool
-  enable_dns_support               = local.u_bool
-  assign_generated_ipv6_cidr_block = local.u_bool
-  instance_tenancy                 = local.u_string
-  vpn_gateway_amazon_side_asn      = local.u_number
-  global_tags                      = local.u_map
-  vpc_tags                         = local.u_map
-}
-
-module "vpc_vgw" {
-  source = "../../modules/vpc"
-
-  # Inputs that cannot handle unknown values.
-  create_vpc              = true
-  cidr_block              = "10.0.0.0/24"
-  secondary_cidr_blocks   = ["10.0.1.0/24", "10.0.2.0/24"]
-  create_internet_gateway = true
-  create_vpn_gateway      = true # the change
-  security_groups = {
-    sg = {
-      rules = {
-        r = {
-          # Nested attributes that can handle unknown values.
-          type = local.u_sg_rule_type
-        }
-      }
-      # Nested attributes that can handle unknown values.
-      name = local.u_string
-    }
-  }
-
-  # Inputs that can handle unknown values.
-  name                             = local.u_string
-  enable_dns_hostnames             = local.u_bool
-  enable_dns_support               = local.u_bool
-  assign_generated_ipv6_cidr_block = local.u_bool
-  instance_tenancy                 = local.u_string
-  vpn_gateway_amazon_side_asn      = local.u_number
-  global_tags                      = local.u_map
-  vpc_tags                         = local.u_map
-}
-
-module "vpc_vgw_noigw" {
-  source = "../../modules/vpc"
-
-  # Inputs that cannot handle unknown values.
-  create_vpc              = true
-  cidr_block              = "10.0.0.0/24"
-  secondary_cidr_blocks   = ["10.0.1.0/24", "10.0.2.0/24"]
-  create_internet_gateway = false # the change
   create_vpn_gateway      = true
   security_groups = {
     sg = {
@@ -287,37 +197,6 @@ module "vpc_vgw_noigw" {
 
   # Inputs that can handle unknown values.
   name                             = local.u_string
-  enable_dns_hostnames             = local.u_bool
-  enable_dns_support               = local.u_bool
-  assign_generated_ipv6_cidr_block = local.u_bool
-  instance_tenancy                 = local.u_string
-  vpn_gateway_amazon_side_asn      = local.u_number
-  global_tags                      = local.u_map
-  vpc_tags                         = local.u_map
-}
-
-module "novpc_igw_vgw" {
-  source = "../../modules/vpc"
-
-  # Inputs that cannot handle unknown values.
-  create_vpc              = false # the change
-  create_internet_gateway = true
-  create_vpn_gateway      = true
-  security_groups = {
-    sg = {
-      rules = {
-        r = {
-          # Nested attributes that can handle unknown values.
-          type = local.u_sg_rule_type
-        }
-      }
-      # Nested attributes that can handle unknown values.
-      name = local.u_string
-    }
-  }
-
-  # Inputs that can handle unknown values.
-  name                             = module.vpc.name
   enable_dns_hostnames             = local.u_bool
   enable_dns_support               = local.u_bool
   assign_generated_ipv6_cidr_block = local.u_bool
@@ -350,22 +229,13 @@ resource "random_pet" "consume_maps" {
     module.vpc_noigw.routing_cidrs,
     module.vpc_noigw.security_group_ids,
 
-    module.novpc_igw.routing_cidrs,
-    module.novpc_igw.security_group_ids,
+    module.vpc_useigw.routing_cidrs,
+    module.vpc_useigw.security_group_ids,
 
-    module.novpc_noigw_nouse.routing_cidrs,
-    module.novpc_noigw_nouse.security_group_ids,
+    module.vpc_novgw.routing_cidrs,
+    module.vpc_novgw.security_group_ids,
 
-    module.novpc_noigw.routing_cidrs,
-    module.novpc_noigw.security_group_ids,
-
-    module.vpc_vgw.routing_cidrs,
-    module.vpc_vgw.security_group_ids,
-
-    module.vpc_vgw_noigw.routing_cidrs,
-    module.vpc_vgw_noigw.security_group_ids,
-
-    module.novpc_igw_vgw.routing_cidrs,
-    module.novpc_igw_vgw.security_group_ids,
+    module.novpc.routing_cidrs,
+    module.novpc.security_group_ids,
   )
 }
