75cd7bfe69b1974ea06b23294247b30cce1ccf77
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2021-11-09 17:28:29 +0100

    feat(vmseries): add output `network_interfaces`
    The output is usable for a Load Balancer Target Group working in `"ip"`
    mode instead of the usual `"instance"` mode (the `target_type` input).
    
    The `"ip"` mode is specifically required for panos overlay routing
    scenarios. Generally, it is required for loadbalancing any non-primary
    interface of a VM on EC2.
    
    commit-id:b7436653

--- modules/vmseries/outputs.tf
+++ modules/vmseries/outputs.tf
@@ -4,3 +4,16 @@ output "firewalls" {
     k => f
   }
 }
+
+output "raw_network_interfaces" {
+  value = aws_network_interface.this
+}
+
+output "network_interfaces" {
+  value = { for k, v in local.interfaces : k => merge(v,
+    {
+      id         = aws_network_interface.this[k].id
+      private_ip = aws_network_interface.this[k].private_ip
+    })
+  }
+}
