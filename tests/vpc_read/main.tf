1cf0c1f7abc6191ecd78648f67e88365343b6618
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2021-11-23 10:54:13 +0100

    test add vpc_read testcases
    Test the scenario with brownfield VPC without the IGW.

--- tests/vpc_read/main.tf
+++ tests/vpc_read/main.tf
@@ -27,7 +27,7 @@ module "vpc" {
 
   name                    = local.vpc_name
   create_vpc              = true
-  create_internet_gateway = true
+  create_internet_gateway = false
   create_vpn_gateway      = true
   cidr_block              = "10.0.0.0/16"
   secondary_cidr_blocks   = ["10.4.0.0/16", "10.5.0.0/16", "10.6.0.0/16"]
@@ -40,6 +40,24 @@ module "vpc_read" {
 
   create_vpc              = false
   name                    = module.vpc.name
+  create_internet_gateway = false
+  use_internet_gateway    = false
+}
+
+module "vpc_read_igw_create" {
+  source = "../../modules/vpc"
+
+  create_vpc              = false
+  name                    = module.vpc.name
+  create_internet_gateway = true
+  use_internet_gateway    = false
+}
+
+module "vpc_read_igw_read" {
+  source = "../../modules/vpc"
+
+  create_vpc              = false
+  name                    = module.vpc_read_igw_create.name
   create_internet_gateway = false
   use_internet_gateway    = true
 }
@@ -60,4 +78,20 @@ output "is_vpc_read_cidr_block_correct" {
 
 output "is_vpc_read_name_correct" {
   value = (module.vpc_read.name == local.vpc_name)
+}
+
+output "is_vpc_read_igw_create_cidr_block_correct" {
+  value = (module.vpc_read_igw_create.vpc.cidr_block == "10.0.0.0/16")
+}
+
+output "is_vpc_read_igw_create_name_correct" {
+  value = (module.vpc_read_igw_create.name == local.vpc_name)
+}
+
+output "is_vpc_read_igw_read_cidr_block_correct" {
+  value = (module.vpc_read_igw_read.vpc.cidr_block == "10.0.0.0/16")
+}
+
+output "is_vpc_read_igw_read_name_correct" {
+  value = (module.vpc_read_igw_read.name == local.vpc_name)
 }
