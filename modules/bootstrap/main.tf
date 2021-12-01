5b407ddcfccc58b028871fad524a7384627fd944
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2021-12-01 15:58:36 +0100

    fix bootstrap remove generated init-cfg
    Closes #121
    
    Remove the templated init-cfg.txt file which the module always creates.
    
    commit-id:0f05e6a5

--- modules/bootstrap/main.tf
+++ modules/bootstrap/main.tf
@@ -17,26 +17,6 @@ resource "aws_s3_bucket_object" "bootstrap_dirs" {
   content = "/dev/null"
 }
 
-resource "aws_s3_bucket_object" "init_cfg" {
-  count = contains(fileset("${path.root}/files", "**"), "config/init-cfg.txt") ? 0 : 1
-
-  bucket = aws_s3_bucket.this.id
-  key    = "config/init-cfg.txt"
-  content = templatefile("${path.module}/init-cfg.txt.tmpl",
-    {
-      "hostname"         = var.hostname,
-      "panorama-server"  = var.panorama-server,
-      "panorama-server2" = var.panorama-server2,
-      "tplname"          = var.tplname,
-      "dgname"           = var.dgname,
-      "dns-primary"      = var.dns-primary,
-      "dns-secondary"    = var.dns-secondary,
-      "vm-auth-key"      = var.vm-auth-key,
-      "op-command-modes" = var.op-command-modes
-    }
-  )
-}
-
 resource "aws_s3_bucket_object" "bootstrap_files" {
   for_each = fileset("${path.root}/files", "**")
 
