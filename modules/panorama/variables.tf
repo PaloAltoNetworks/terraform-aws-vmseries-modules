2c58e95c6acada342c3e84ee959ec2757589e253
Damian Fedeczko <dfedeczko@paloaltonetworks.com>
2021-12-09 15:26:56 +0100

    Fix panorama_version cli lookup, update docs

--- modules/panorama/variables.tf
+++ modules/panorama/variables.tf
@@ -24,9 +24,11 @@ variable "product_code" {
 variable "panorama_version" {
   description = <<-EOF
   Panorama PAN-OS Software version. List published images with: 
-  `aws ec2 describe-images \
-      --filters "Name=product-code,Values=eclz7j04vu9lf8ont8ta3n17o" "Name=name,Values=Panorama-AWS*" \
-      --output json --query "Images[].Description" | grep -o 'Panorama-AWS-.*' | tr -d '",'`
+  ```
+  aws ec2 describe-images \\
+  --filters "Name=product-code,Values=eclz7j04vu9lf8ont8ta3n17o" "Name=name,Values=Panorama-AWS*" \\
+  --output json --query "Images[].Description" \| grep -o 'Panorama-AWS-.*' \| tr -d '",'
+  ```
   EOF
   default     = "10.0.6"
   type        = string
