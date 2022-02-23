55f03acc0f063f8ffd280fe623d324728f8107d9
michalbil <mbilski@paloaltonetworks.com>
2022-02-23 17:01:10 +0100

    Fix output in standalone example

--- examples/standalone_vmseries_with_userdata_bootstrap/outputs.tf
+++ examples/standalone_vmseries_with_userdata_bootstrap/outputs.tf
@@ -1,4 +1,4 @@
 output "public_ips" {
   description = "Map of public IPs created within the module."
-  value       = { for k, v in module.vmseries : k => v.public_ip }
+  value       = { for k, v in module.vmseries : k => v.public_ips }
 }
