74a539ebdfc10fe8e08ea36583fe72f17dfa40d0
michalbil <92343355+michalbil@users.noreply.github.com>
2022-02-23 07:28:41 +0100

    feat(vmseries): Add output with created public IPs (#171)

--- modules/vmseries/outputs.tf
+++ modules/vmseries/outputs.tf
@@ -6,3 +6,8 @@ output "interfaces" {
   description = "Map of VM-Series network interfaces. The entries are `aws_network_interface` objects."
   value       = aws_network_interface.this
 }
+
+output "public_ips" {
+  description = "Map of public IPs created within the module."
+  value       = { for k, v in aws_eip.this : k => v.public_ip }
+}
