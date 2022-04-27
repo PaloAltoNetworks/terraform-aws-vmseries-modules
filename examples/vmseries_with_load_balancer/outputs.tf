e71fb6c967931c2a1a207194a03e7dca698cd3af
Łukasz Pawlęga <lpawlega@paloaltonetworks.com>
2022-04-27 15:28:19 +0200

    add outputs

--- examples/vmseries_with_load_balancer/outputs.tf
+++ examples/vmseries_with_load_balancer/outputs.tf
@@ -3,16 +3,6 @@ output "fw_public_ips" {
   value       = { for k, v in module.vmseries : k => v.public_ips }
 }
 
-# output "public_lb" {
-#   value = {
-#     fqdn        = module.public_nlb.lb_fqdn
-#     private_ips = module.public_nlb.lb_private_ips
-#   }
-# }
-
-# output "private_lb" {
-#   value = {
-#     fqdn        = module.private_nlb.lb_fqdn
-#     private_ips = module.private_nlb.lb_private_ips
-#   }
-# }
+output "public_lb_fqdn" {
+  value = module.public_nlb.lb_fqdn
+}
