e4ce01039f3687ed650615e19ea4b2088eec93c0
Łukasz Pawlęga <lpawlega@paloaltonetworks.com>
2022-04-29 12:07:07 +0200

    setting back default action for listener to 503

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -202,8 +202,7 @@ resource "aws_lb_listener" "this" {
     type = "fixed-response"
     fixed_response {
       content_type = "text/plain"
-      message_body = "Fixed response content"
-      status_code  = "200"
+      status_code  = "503"
     }
   }
 
