98f82f45f14d1fb19935ea317f463782f1cd0c3b
Łukasz Pawlęga <lpawlega@paloaltonetworks.com>
2022-06-30 10:33:35 +0200

    removing debug

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,5 +5,5 @@
 set -euo pipefail
 
 pre-commit run --all-files terraform_fmt 
-pre-commit run --show-diff-on-failure --all-files terraform_docs 
+pre-commit run --all-files terraform_docs 
 pre-commit run --all-files terraform_tflint
