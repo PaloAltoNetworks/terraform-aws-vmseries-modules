38016520e518db1d48799b4ac9c7eb93bd7e7eb3
Sebastian Czech <sczech@paloaltonetworks.com>
2023-01-09 09:27:16 +0100

    Clean code after using new test skeleton

--- tests/vpc_route/main_test.go
+++ tests/vpc_route/main_test.go
@@ -6,32 +6,9 @@ import (
 	"github.com/PaloAltoNetworks/terraform-aws-vmseries-modules/tests/internal/testskeleton"
 	"github.com/gruntwork-io/terratest/modules/logger"
 	"github.com/gruntwork-io/terratest/modules/terraform"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestOutputWhileCreatingManagedPrefixListForVpcRouteModule(t *testing.T) {
-	// given
-	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-		TerraformDir: ".",
-		Logger:       logger.Default,
-		Lock:         true,
-		Upgrade:      true,
-	})
-	defer terraform.Destroy(t, terraformOptions)
-
-	// when
-	terraform.InitAndApply(t, terraformOptions)
-	routesCidr := terraform.OutputList(t, terraformOptions, "routes_cidr")
-	routesMpl := terraform.OutputList(t, terraformOptions, "routes_mpl")
-
-	// then
-	assert.NotEmpty(t, routesCidr)
-	assert.NotEmpty(t, routesMpl)
-	assert.Equal(t, 2, len(routesCidr))
-	assert.Equal(t, 1, len(routesMpl))
-}
-
-func TestModuleOutputs(t *testing.T) {
 	var terraformOptions = terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		TerraformDir: ".",
 		Vars: map[string]interface{}{
