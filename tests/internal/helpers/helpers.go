adb85fe9ba1caa6c1c8d9536727a6a65546c0c8b
Sebastian Czech <sczech@paloaltonetworks.com>
2023-01-24 15:25:57 +0100

    Refactor helper functions

--- tests/internal/helpers/helpers.go
+++ tests/internal/helpers/helpers.go
@@ -19,20 +19,20 @@ func CheckHttpGetWebApp(t *testing.T, outputValue string) bool {
 
 	// Check in the loop if application is healthy
 	for i := 1; i <= numberOfRetries && !urlHealthy; i++ {
-		// HTTP GET
-		time.Sleep(sleepBetweenRetry)
-
 		//TODO: Check if client can replace :22
 		//client := http.Client{
 		//	Timeout: 15 * time.Second,
 		//}
+
+		// Send HTTP GET
 		resp, err := http.Get(outputValue)
 
 		// Display errors, if there were any, or HTTPS status code, if no errors
 		if err != nil {
 			t.Logf("Waiting for application (%d/%d)... error HTTP GET: %v\n", i, numberOfRetries, err)
+			time.Sleep(sleepBetweenRetry)
 		} else {
-			t.Logf("Application HTTP GET status code: %v", resp.StatusCode)
+			t.Logf("Application HTTP GET %v status code: %v", outputValue, resp.StatusCode)
 			urlHealthy = resp.StatusCode == 200
 		}
 	}
@@ -40,7 +40,24 @@ func CheckHttpGetWebApp(t *testing.T, outputValue string) bool {
 }
 
 func CheckTcpPortOpened(t *testing.T, outputValue string) bool {
-	// verify if port is opened (defined with address in outputValue)
-	_, err := net.DialTimeout("tcp", outputValue, 5*time.Second)
-	return err == nil
+	// Define how many retries and how often to do in order to check if port is opened (defined with address in outputValue)
+	sleepBetweenRetry := 15 * time.Second
+	numberOfRetries := 60
+	urlHealthy := false
+
+	// Check in the loop if port is opened
+	for i := 1; i <= numberOfRetries && !urlHealthy; i++ {
+		// Check TCP port
+		_, err := net.DialTimeout("tcp", outputValue, 5*time.Second)
+
+		// Display errors, if there were any
+		if err != nil {
+			t.Logf("Waiting for TCP port opened (%d/%d)... error: %v\n", i, numberOfRetries, err)
+			time.Sleep(sleepBetweenRetry)
+		} else {
+			t.Logf("TCP %v port opened", outputValue)
+			urlHealthy = true
+		}
+	}
+	return urlHealthy
 }
--- tests/vmseries/main_test.go
+++ tests/vmseries/main_test.go
@@ -9,6 +9,47 @@ import (
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
 
+func TestOutputForModuleVmseriesWithFullVariables(t *testing.T) {
+	// define options for Terraform
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: ".",
+		VarFiles:     []string{"terraform_full.tfvars"},
+		Vars: map[string]interface{}{
+			"name_prefix": "terratest_module_vmseries_",
+		},
+		Logger:               logger.Default,
+		Lock:                 true,
+		Upgrade:              true,
+		SetVarsAfterVarFiles: true,
+	})
+
+	// prepare list of items to check
+	assertList := []testskeleton.AssertExpression{
+		// check VM-Series URL
+		{
+			OutputName: "vmseries_url",
+			Operation:  "NotEmpty",
+		},
+		// check access to login page in web UI for VM-Series
+		{
+			Operation:  "CheckFunctionWithOutput",
+			Check:      helpers.CheckHttpGetWebApp,
+			OutputName: "vmseries_url",
+			Message:    "After bootstrapping, which takes few minutes, web UI for VM-Series should be accessible",
+		},
+		// check access via SSH to VM-Series
+		{
+			Operation:  "CheckFunctionWithOutput",
+			Check:      helpers.CheckTcpPortOpened,
+			OutputName: "vmseries_ssh",
+			Message:    "After bootstrapping, which takes few minutes, SSH for VM-Series should be accessible",
+		},
+	}
+
+	// deploy test infrastructure and verify outputs and check if there are no planned changes after deployment
+	testskeleton.DeployInfraCheckOutputsVerifyChanges(t, terraformOptions, assertList)
+}
+
 func TestOutputForModuleVmseriesWithMinimumVariables(t *testing.T) {
 	// define options for Terraform
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
