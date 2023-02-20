2f5237afff0aa1efba2a328bb8182364832cc071
Horia Gunica <Horia.Gunica@gmail.com>
2023-02-20 13:17:15 +0200

    CKV_AWS_135

--- examples/tgw_inbound_with_alb_nlb/application.tf
+++ examples/tgw_inbound_with_alb_nlb/application.tf
@@ -116,6 +116,7 @@ resource "aws_instance" "app_vm" {
 
   subnet_id              = module.app_subnet_sets["appl"].subnets[each.value.az].id
   vpc_security_group_ids = [module.app_vpc.security_group_ids["app_example"]]
+  ebs_optimized          = true
   metadata_options {
     http_endpoint = "enabled"
     http_tokens   = "required"
