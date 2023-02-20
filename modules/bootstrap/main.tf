4d8ec73a5ea751c44235e0e979641d99024d8530
Horia Gunica <Horia.Gunica@gmail.com>
2023-02-20 14:46:18 +0200

    CKV_AWS_19

--- modules/bootstrap/main.tf
+++ modules/bootstrap/main.tf
@@ -25,6 +25,13 @@ resource "aws_s3_bucket" "this" {
   versioning {
     enabled = true
   }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
 
 resource "aws_s3_bucket_acl" "this" {
