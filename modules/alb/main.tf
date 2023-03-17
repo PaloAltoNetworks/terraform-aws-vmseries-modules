a0857f5879e5167b5ef7a89e1075659a71fb59d9
Mariusz Gebala <mgebala@paloaltonetworks.com>
2023-03-17 16:26:38 +0100

    Added availability_zone parameter to target group

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -238,9 +238,10 @@ resource "aws_lb_target_group" "this" {
 resource "aws_lb_target_group_attachment" "this" {
   for_each = local.listener_tg_attachments
 
-  target_group_arn = aws_lb_target_group.this[each.value.listener_tg_name].arn
-  target_id        = each.value.ip
-  port             = each.value.port
+  target_group_arn  = aws_lb_target_group.this[each.value.listener_tg_name].arn
+  target_id         = each.value.ip
+  port              = each.value.porta
+  availability_zone = "all"
 }
 # ######################## #
 
