ebf22bafdf571c20f11c28e2c41e7555fc56ec85
Sebastian Czech <sczech@paloaltonetworks.com>
2023-07-13 08:36:39 +0200

    Fix CKV_AWS_19

--- modules/bootstrap/main.tf
+++ modules/bootstrap/main.tf
@@ -26,6 +26,14 @@ resource "aws_s3_bucket" "this" {
   versioning {
     enabled = true
   }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
 
 resource "aws_s3_bucket_ownership_controls" "this" {
