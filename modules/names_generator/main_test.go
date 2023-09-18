64c50881dcda00c2a1be5283941a05741a4d040d
Sebastian Czech <sczech@paloaltonetworks.com>
2023-09-18 16:03:25 +0200

    Remove Makefile, use Terratest to validate module

---
+++ modules/names_generator/main_test.go
@@ -0,0 +1,11 @@
+package names_generator
+
+import (
+	"testing"
+
+	"github.com/PaloAltoNetworks/terraform-modules-vmseries-tests-skeleton/pkg/testskeleton"
+)
+
+func TestValidate(t *testing.T) {
+	testskeleton.ValidateCode(t, nil)
+}
