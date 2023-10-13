1d4f99befd70bb90bcb66cfac0af033b6305c5e5
Sean Youngberg <sean.youngberg@gmail.com>
2023-10-13 12:27:20 -0400

    initial commit for combined ob/ew with separate dedicated inbound. tested working. Removed subinterface associations and no bootstrap or panorama for now

---
+++ deployments/combined-with-dedicated-inbound/versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.17"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
